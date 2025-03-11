c7320e17444794873e1207fd574f6ccf02997f0d
Ala Williams <alicja.m.williams@gmail.com>
2025-03-11 10:19:32 +0200

    app functionality and styling added

--- js/index.js
+++ js/index.js
@@ -1,2 +1,17 @@
 const input = document.querySelector('.task-input');
 const taskList = document.querySelector('.task-list');
+
+
+input.addEventListener('keypress', (e) => {
+ const newTask = input.value.trim();
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    if (newTask !== '') {
+      const task = document.createElement('li');
+      task.innerHTML = newTask;
+      taskList.appendChild(task);
+      task.className = 'item';
+      input.value = ''
+    }
+  }
+})
