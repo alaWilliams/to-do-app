289276823caae59056d279dc8f3ea15dcb1a8f23
Ala Williams <alicja.m.williams@gmail.com>
2025-03-20 08:58:07 +0200

    routing done

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const { query } = require('./helpers/db.js');
 
+const  todoRouter = require('./routes/todo.js')
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -10,43 +12,45 @@ app.use(express.urlencoded({ extended: false }));
 
 const port = process.env.PORT;
 
-app.get('/', async (req, res) => {
-	console.log(query);
-	try {
-		const result = await query('select * from task');
-		const rows = result.rows ? result.rows : [];
-		res.status(200).json(rows);
-	} catch (error) {
-		console.log(error);
-		res.statusMessage = error;
-		res.status(500).json({ error: error });
-	}
-});
+app.use('/api', todoRouter)
 
-app.listen(port);
+// app.get('/', async (req, res) => {
+// 	console.log(query);
+// 	try {
+// 		const result = await query('select * from task');
+// 		const rows = result.rows ? result.rows : [];
+// 		res.status(200).json(rows);
+// 	} catch (error) {
+// 		console.log(error);
+// 		res.statusMessage = error;
+// 		res.status(500).json({ error: error });
+// 	}
+// });
 
-app.post('/new', async (req, res) => {
-	try {
-		const result = await query(
-			'insert into task (description) values ($1) returning *',
-			[req.body.description]
-		);
-		res.status(200).json({ id: result.rows[0].id });
-	} catch (error) {
-		console.log(error);
-		res.statusMessage = error;
-		res.status(500).json({ error: error });
-	}
-});
+app.listen(port, () => {console.log(`Server running on port ${port}`)});
 
-app.delete('/delete/:id', async (req, res) => {
-	const id = Number(req.params.id);
-	try {
-		const result = await query('delete from task where id = $1', [id]);
-		res.status(200).json({ id: id });
-	} catch (error) {
-		console.log(error);
-		res.statusMessage = error;
-		res.status(500).json({ error: error });
-	}
-});
+// app.post('/new', async (req, res) => {
+// 	try {
+// 		const result = await query(
+// 			'insert into task (description) values ($1) returning *',
+// 			[req.body.description]
+// 		);
+// 		res.status(200).json({ id: result.rows[0].id });
+// 	} catch (error) {
+// 		console.log(error);
+// 		res.statusMessage = error;
+// 		res.status(500).json({ error: error });
+// 	}
+// });
+
+// app.delete('/delete/:id', async (req, res) => {
+// 	const id = Number(req.params.id);
+// 	try {
+// 		const result = await query('delete from task where id = $1', [id]);
+// 		res.status(200).json({ id: id });
+// 	} catch (error) {
+// 		console.log(error);
+// 		res.statusMessage = error;
+// 		res.status(500).json({ error: error });
+// 	}
+// });
